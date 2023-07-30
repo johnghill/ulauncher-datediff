fe38b4d7e5b68c4a3550f89912cb7d4f62b1b1ac
john@nzxt <j.hill@herts.ac.uk>
2023-07-30 17:10:23 +0100

    FIXED class import in main.py

--- main.py
+++ main.py
@@ -1,29 +1,39 @@
-import subprocess
-from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
-from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
-from ulauncher.api.shared.event import KeywordQueryEvent
+from ulauncher.api.client.Extension import Extension
 from ulauncher.api.client.EventListener import EventListener
+from ulauncher.api.shared.event import KeywordQueryEvent
+from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
+from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
+from ulauncher.api.shared.action.HideWindowAction import HideWindowAction
+import subprocess
+import logging
+
+logger = logging.getLogger(__name__)
 
 def days_between(d1, d2):
     cmd = ['bash', '-c', f'd1=$(date -d "{d1}" +%s); d2=$(date -d "{d2}" +%s); echo $(( (d2 - d1) / 86400 ))']
     result = subprocess.check_output(cmd).decode('utf-8').strip()
     return result
 
-class DaysBetweenDatesExtension(EventListener):
+class DaysBetweenDatesExtension(Extension):
+
+    def __init__(self):
+        super(DaysBetweenDatesExtension, self).__init__()
+        self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
+
+class KeywordQueryEventListener(EventListener):
 
     def on_event(self, event, extension):
-        if isinstance(event, KeywordQueryEvent):
-            query = event.get_argument()
-            if not query:
-                return RenderResultListAction([
-                    ExtensionResultItem(icon='images/calendar-days-solid.svg', name='Enter two dates separated by a comma', on_enter=None)
-                ])
-
-            date1, date2 = [date.strip() for date in query.split(',')]
-            result = days_between(date1, date2)
+        query = event.get_argument()
+        if not query:
             return RenderResultListAction([
-                ExtensionResultItem(icon='images/calendar-days-solid.svg', name=f'Days between {date1} and {date2}: {result}', on_enter=None)
+                ExtensionResultItem(icon='images/calendar-days-solid.svg', name='Enter two dates separated by a comma', on_enter=HideWindowAction())
             ])
+
+        date1, date2 = [date.strip() for date in query.split(',')]
+        result = days_between(date1, date2)
+        return RenderResultListAction([
+            ExtensionResultItem(icon='images/calendar-days-solid.svg', name=f'Days between {date1} and {date2}: {result}', on_enter=HideWindowAction())
+        ])
 
 if __name__ == '__main__':
     DaysBetweenDatesExtension().run()
